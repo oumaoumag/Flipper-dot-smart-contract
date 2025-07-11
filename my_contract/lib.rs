3b4f5b61276b16243a54d0159808b640b4e4694a
oumaoumag <ouma.godwin10@gmail.com>
2025-07-11 07:08:35 +0300

    Add conract address as comments

--- my_contract/lib.rs
+++ my_contract/lib.rs
@@ -1,3 +1,6 @@
+//Address -  0x9621dde636de098b43efb0fa9b61facfe328f99d
+// Contract 0xe2a313e210a6ec1d5a9c0806545670f2e6264f86
+
 #![cfg_attr(not(feature = "std"), no_std, no_main)]
 
 #[ink::contract]
